5caafd64b4273c060c3a68098d7221c601d4268b
josemasster <joseh.jgvr55@gmail.com>
2024-04-11 12:31:07 -0500

    hotfix:despliegue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -26,6 +26,8 @@ export const environment = {
     "@udistrital/sga-derechos-pecuniarios-mf": "//localhost:4208/main.js",
     "@udistrital/sga-notas-mf": "//localhost:4213/main.js",
     "@udistrital/sga-gestion-horarios-mf": "//localhost:4214/main.js",
-    "@udistrital/calendario-academico-mf":"https://pruebassgacalendarioacademico.portaloas.udistrital.edu.co/main.js"
+    "@udistrital/sga-calendarioacademico-mf":"//localhost:4203/main.js"
   }
+
+  
 };
